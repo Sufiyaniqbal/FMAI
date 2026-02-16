a8c7993659a5ac2e76c1fb895516046dc2fcb380
SufiyanComEnv <sufiyaniqbal29@gmail.com>
2026-02-16 10:20:14 +0400

    update analyz.py

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,1 +1,10 @@
-print("Python environment is ready!")
+import pandas as pd
+
+customers = pd.read_csv('../data/customers.csv')
+invoices = pd.read_csv('../data/invoices.csv')
+
+print("Customers:")
+print(customers.head())
+
+print("Invoices:")
+print(invoices.head())
